3a37b83539ebae07de4df6242f0fa918fd24494c
Hamida Sonia Dewi <hamidasonia11@gmail.com>
2024-12-10 15:44:21 +0700

    rename project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,17 +20,19 @@
     <header class="wrapper bg-soft-primary">
       <nav class="navbar navbar-expand-lg center-nav transparent position-absolute navbar-dark caret-none">
         <div class="container flex-lg-row flex-nowrap align-items-center">
-          <div class="navbar-brand w-100" style="display: flex; align-items: center; justify-content: center; color: white;">
+          <div class="navbar-brand w-100"
+            style="display: flex; align-items: center; justify-content: center; color: white;">
             <a href="#" style="text-decoration: none; color: inherit; display: flex; align-items: center;">
               <div class="row" style="display: flex ; flex-direction: column; align-items: center; gap: 10px;">
-                <img src="{{ asset('img/photos/logo-lugas.png') }}" alt="" style="width: 90px; height: 50px; object-fit: contain;">
+                <img src="{{ asset('img/photos/logo-lugas.png') }}" alt=""
+                  style="width: 90px; height: 50px; object-fit: contain;">
                 <span style="line-height: 1;">CV. SOLUSI UNGGASINDO</span>
               </div>
             </a>
           </div>
           <div class="navbar-collapse offcanvas offcanvas-nav offcanvas-start">
             <div class="offcanvas-header d-lg-none">
-              <h3 class="text-white fs-30 mb-0">Sandbox</h3>
+              <h3 class="text-white fs-30 mb-0">LUGAS</h3>
               <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
                 aria-label="Close"></button>
             </div>
@@ -53,7 +55,7 @@
                   <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">FAQ</a>
                   <!--/.dropdown-menu -->
                 </li>
-                
+
               </ul>
               <!-- /.navbar-nav -->
               <div class="offcanvas-footer d-lg-none">
@@ -100,7 +102,8 @@
         </div>
         <div class="offcanvas-body pb-6">
           <div class="widget mb-8">
-            <p>LUGAS adalah konsultan pembangunan dan manajemen kandang ayam petelur Indonesia yang mengedepankan efisiensi dalam produksi </p>
+            <p>LUGAS adalah konsultan pembangunan dan manajemen kandang ayam petelur Indonesia yang mengedepankan
+              efisiensi dalam produksi </p>
           </div>
           <!-- /.widget -->
           <div class="widget mb-8">
@@ -236,12 +239,12 @@
             </div>
             <div class="overlap-grid overlap-grid-2">
               <div class="item">
-                <figure class="rounded shadow"><img src="{{ asset('img/photos/flock-chickens-fence.jpg') }}"
-                     alt=""></figure>
+                <figure class="rounded shadow"><img src="{{ asset('img/photos/flock-chickens-fence.jpg') }}" alt="">
+                </figure>
               </div>
               <div class="item">
                 <figure class="rounded shadow"><img src="{{ asset('img/photos/hens-factory-chicken-cages.jpg') }}"
-                     alt=""></figure>
+                    alt=""></figure>
               </div>
             </div>
           </div>
@@ -321,14 +324,18 @@
             <h2 class="display-6 mb-3">Bagaimana Mewujudkannya?</h2>
             <p class="lead fs-lg pe-lg-5">Berikut Tahapan Dalam Mewujudkan Mimpi Menjadi Peternak Ayam Petelur</p>
             <h4 class="mb-1">01. Proyeksi peluang bisnis</h4>
-            <p class="mb-0">Memetakan kebutuhan domestik, membuat perencanaan keuangan, ketersediaan pakan, dan memastikan distribution channel</p>
+            <p class="mb-0">Memetakan kebutuhan domestik, membuat perencanaan keuangan, ketersediaan pakan, dan
+              memastikan distribution channel</p>
             <h4 class="mb-1">02. Membuat kandang</h4>
-            <p class="mb-0">Perencanaan kandang yang disesuaikan dengan anggaran, kebutuhan, geografis dan alat yang diperlukan</p>
+            <p class="mb-0">Perencanaan kandang yang disesuaikan dengan anggaran, kebutuhan, geografis dan alat yang
+              diperlukan</p>
             <h4 class="mb-1">03. Manajemen kandang yang baik</h4>
-            <p class="mb-0"> Memastikan operasional kandang dapat berjalan dengan baik, Anda dapat menerapkan sistem kerja kami yang telah terbukti efisien</p>
+            <p class="mb-0"> Memastikan operasional kandang dapat berjalan dengan baik, Anda dapat menerapkan sistem
+              kerja kami yang telah terbukti efisien</p>
             <h4 class="mb-1">04. Menghasilkan produk</h4>
-            <p class="mb-0">Dengan sistem manajemen yang baik, Anda dapat menghasilkan produk yang baik dan sesuai linimasa</p>
-            
+            <p class="mb-0">Dengan sistem manajemen yang baik, Anda dapat menghasilkan produk yang baik dan sesuai
+              linimasa</p>
+
           </div>
           <!--/column -->
         </div>
@@ -340,13 +347,16 @@
     <section id="snippet-1" class="wrapper bg-light wrapper-border">
       <div class="container pt-15 pt-md-17 pb-13 pb-md-15">
         <h2 class="display-4 mb-3 text-center">Portfolio</h2>
-        <p class="lead fs-lg mb-10 text-center px-md-16 px-lg-21 px-xl-0">Project Sukses Kami Dalam Membangun Kandang</p>
-        <div class="swiper-container blog grid-view mb-6" data-margin="30" data-dots="true" data-items-xl="3" data-items-md="2" data-items-xs="1">
+        <p class="lead fs-lg mb-10 text-center px-md-16 px-lg-21 px-xl-0">Project Sukses Kami Dalam Membangun Kandang
+        </p>
+        <div class="swiper-container blog grid-view mb-6" data-margin="30" data-dots="true" data-items-xl="3"
+          data-items-md="2" data-items-xs="1">
           <div class="swiper">
             <div class="swiper-wrapper">
               <div class="swiper-slide">
                 <article>
-                  <figure class="overlay overlay-1 hover-scale rounded mb-5"><a href="#"> <img src="{{ asset('img/photos/b4.jpg') }}" alt="" /></a>
+                  <figure class="overlay overlay-1 hover-scale rounded mb-5"><a href="#"> <img
+                        src="{{ asset('img/photos/b4.jpg') }}" alt="" /></a>
                     <figcaption>
                       <h5 class="from-top mb-0">Read More</h5>
                     </figcaption>
@@ -356,7 +366,8 @@
                       <a href="#" class="hover" rel="category">Kandang</a>
                     </div>
                     <!-- /.post-category -->
-                    <h2 class="post-title h3 mt-1 mb-3"><a class="link-dark" href="../../blog-post.html">Ligula tristique quis risus</a></h2>
+                    <h2 class="post-title h3 mt-1 mb-3"><a class="link-dark" href="../../blog-post.html">Ligula
+                        tristique quis risus</a></h2>
                   </div>
                   <!-- /.post-header -->
                   <div class="post-footer">
@@ -373,7 +384,8 @@
               <!--/.swiper-slide -->
               <div class="swiper-slide">
                 <article>
-                  <figure class="overlay overlay-1 hover-scale rounded mb-5"><a href="#"> <img src="{{ asset('img/photos/b5.jpg') }}" alt="" /></a>
+                  <figure class="overlay overlay-1 hover-scale rounded mb-5"><a href="#"> <img
+                        src="{{ asset('img/photos/b5.jpg') }}" alt="" /></a>
                     <figcaption>
                       <h5 class="from-top mb-0">Lebih lanjut</h5>
                     </figcaption>
@@ -383,7 +395,8 @@
                       <a href="#" class="hover" rel="category">Kandang</a>
                     </div>
                     <!-- /.post-category -->
-                    <h2 class="post-title h3 mt-1 mb-3"><a class="link-dark" href="../../blog-post.html">Nullam id dolor elit id nibh</a></h2>
+                    <h2 class="post-title h3 mt-1 mb-3"><a class="link-dark" href="../../blog-post.html">Nullam id dolor
+                        elit id nibh</a></h2>
                   </div>
                   <!-- /.post-header -->
                   <div class="post-footer">
@@ -400,7 +413,8 @@
               <!--/.swiper-slide -->
               <div class="swiper-slide">
                 <article>
-                  <figure class="overlay overlay-1 hover-scale rounded mb-5"><a href="#"> <img src="{{ asset('img/photos/b6.jpg') }}" alt="" /></a>
+                  <figure class="overlay overlay-1 hover-scale rounded mb-5"><a href="#"> <img
+                        src="{{ asset('img/photos/b6.jpg') }}" alt="" /></a>
                     <figcaption>
                       <h5 class="from-top mb-0">Lebih lanjut</h5>
                     </figcaption>
@@ -410,7 +424,8 @@
                       <a href="#" class="hover" rel="category">Kandang</a>
                     </div>
                     <!-- /.post-category -->
-                    <h2 class="post-title h3 mt-1 mb-3"><a class="link-dark" href="../../blog-post.html">Ultricies fusce porta elit</a></h2>
+                    <h2 class="post-title h3 mt-1 mb-3"><a class="link-dark" href="../../blog-post.html">Ultricies fusce
+                        porta elit</a></h2>
                   </div>
                   <!-- /.post-header -->
                   <div class="post-footer">
@@ -427,7 +442,8 @@
               <!--/.swiper-slide -->
               <div class="swiper-slide">
                 <article>
-                  <figure class="overlay overlay-1 hover-scale rounded mb-5"><a href="#"> <img src="{{ asset('img/photos/b7.jpg') }}" alt="" /></a>
+                  <figure class="overlay overlay-1 hover-scale rounded mb-5"><a href="#"> <img
+                        src="{{ asset('img/photos/b7.jpg') }}" alt="" /></a>
                     <figcaption>
                       <h5 class="from-top mb-0">Lebih lanjut</h5>
                     </figcaption>
@@ -437,7 +453,8 @@
                       <a href="#" class="hover" rel="category">Kandang</a>
                     </div>
                     <!-- /.post-category -->
-                    <h2 class="post-title h3 mt-1 mb-3"><a class="link-dark" href="../../blog-post.html">Morbi leo risus porta eget</a></h2>
+                    <h2 class="post-title h3 mt-1 mb-3"><a class="link-dark" href="../../blog-post.html">Morbi leo risus
+                        porta eget</a></h2>
                   </div>
                   <div class="post-footer">
                     <ul class="post-meta">
@@ -463,55 +480,60 @@
     </section>
     <!-- /section -->
 
-  <!-- section -->
-  <section class="wrapper bg-gradient-primary">
-    <div class="container pt-10 pt-md-14 pb-8 text-center">
-      <div class="row gx-lg-8 gx-xl-12 gy-10 align-items-center">
-        <div class="col-lg-7">
-          <figure><img class="w-auto" src="{{ asset('img/illustrations/i2.png') }}" srcset="{{ asset('img/illustrations/i2.png') }}" alt="" /></figure>
-        </div>
-        <!-- /column -->
-        <div class="col-md-10 offset-md-1 offset-lg-0 col-lg-5 text-center text-lg-start">
-          <h1 class="display-1 mb-5 mx-md-n5 mx-lg-0">Proyeksi Kami</h1>
-          <h3 class="lead fs-lg mb-7">Indistri Ayam Petelur Adalah Masa Depan</h3>
-          <span>Dengan semakin tingginya daya konsumsi telur secara nasional dan meningkatnya kebutuhan akan protein terjangkau, maka dibutuhkan peternak-peternak lokal baru yang dapat memenuhi kebutuhan tersebut, maka kami mengajak Anda untuk dapat ikut andil di dalamnya.</></span>
-        </div>
-        <!-- /column -->
-      </div>
-      <!-- /.row -->
-    </div>
-    <!-- /.container -->
-  </section>
-  <!-- /section -->
-  <section class="wrapper bg-light wrapper-border">
-    <div class="container py-14 py-md-16">
-      <p class="text-center mb-8">Partner Kami</p>
-      <div class="swiper-container clients mb-0" data-margin="30" data-dots="false" data-loop="true" data-autoplay="true" data-autoplaytime="1" data-drag="false" data-speed="5000" data-items-xxl="7" data-items-xl="6" data-items-lg="5" data-items-md="4" data-items-xs="2">
-        <div class="swiper">
-          <div class="swiper-wrapper ticker">
-            <div class="swiper-slide px-5"><img src="{{ asset('img/brands/c1.png') }}" alt="" /></div>
-            <div class="swiper-slide px-5"><img src="{{ asset('img/brands/c2.png') }}" alt="" /></div>
-            <div class="swiper-slide px-5"><img src="{{ asset('img/brands/c3.png') }}" alt="" /></div>
-            <div class="swiper-slide px-5"><img src="{{ asset('img/brands/c4.png') }}" alt="" /></div>
-            <div class="swiper-slide px-5"><img src="{{ asset('img/brands/c5.png') }}" alt="" /></div>
-            <div class="swiper-slide px-5"><img src="{{ asset('img/brands/c6.png') }}" alt="" /></div>
-            <div class="swiper-slide px-5"><img src="{{ asset('img/brands/c7.png') }}" alt="" /></div>
-            <div class="swiper-slide px-5"><img src="{{ asset('img/brands/c8.png') }}" alt="" /></div>
-            <div class="swiper-slide px-5"><img src="{{ asset('img/brands/c9.png') }}" alt="" /></div>
-            <div class="swiper-slide px-5"><img src="{{ asset('img/brands/c10.png') }}" alt="" /></div>
-            <div class="swiper-slide px-5"><img src="{{ asset('img/brands/c11.png') }}" alt="" /></div>
+    <!-- section -->
+    <section class="wrapper bg-gradient-primary">
+      <div class="container pt-10 pt-md-14 pb-8 text-center">
+        <div class="row gx-lg-8 gx-xl-12 gy-10 align-items-center">
+          <div class="col-lg-7">
+            <figure><img class="w-auto" src="{{ asset('img/illustrations/i2.png') }}"
+                srcset="{{ asset('img/illustrations/i2.png') }}" alt="" /></figure>
           </div>
-          <!--/.swiper-wrapper -->
+          <!-- /column -->
+          <div class="col-md-10 offset-md-1 offset-lg-0 col-lg-5 text-center text-lg-start">
+            <h1 class="display-1 mb-5 mx-md-n5 mx-lg-0">Proyeksi Kami</h1>
+            <h3 class="lead fs-lg mb-7">Indistri Ayam Petelur Adalah Masa Depan</h3>
+            <span>Dengan semakin tingginya daya konsumsi telur secara nasional dan meningkatnya kebutuhan akan protein
+              terjangkau, maka dibutuhkan peternak-peternak lokal baru yang dapat memenuhi kebutuhan tersebut, maka kami
+              mengajak Anda untuk dapat ikut andil di dalamnya.</></span>
+          </div>
+          <!-- /column -->
         </div>
-        <!-- /.swiper -->
+        <!-- /.row -->
       </div>
-      <!-- /.swiper-container -->
-    </div>
-    <!-- /.container -->
-  </section>
-  <!-- /section -->
+      <!-- /.container -->
+    </section>
     <!-- /section -->
-    
+    <section class="wrapper bg-light wrapper-border">
+      <div class="container py-14 py-md-16">
+        <p class="text-center mb-8">Partner Kami</p>
+        <div class="swiper-container clients mb-0" data-margin="30" data-dots="false" data-loop="true"
+          data-autoplay="true" data-autoplaytime="1" data-drag="false" data-speed="5000" data-items-xxl="7"
+          data-items-xl="6" data-items-lg="5" data-items-md="4" data-items-xs="2">
+          <div class="swiper">
+            <div class="swiper-wrapper ticker">
+              <div class="swiper-slide px-5"><img src="{{ asset('img/brands/c1.png') }}" alt="" /></div>
+              <div class="swiper-slide px-5"><img src="{{ asset('img/brands/c2.png') }}" alt="" /></div>
+              <div class="swiper-slide px-5"><img src="{{ asset('img/brands/c3.png') }}" alt="" /></div>
+              <div class="swiper-slide px-5"><img src="{{ asset('img/brands/c4.png') }}" alt="" /></div>
+              <div class="swiper-slide px-5"><img src="{{ asset('img/brands/c5.png') }}" alt="" /></div>
+              <div class="swiper-slide px-5"><img src="{{ asset('img/brands/c6.png') }}" alt="" /></div>
+              <div class="swiper-slide px-5"><img src="{{ asset('img/brands/c7.png') }}" alt="" /></div>
+              <div class="swiper-slide px-5"><img src="{{ asset('img/brands/c8.png') }}" alt="" /></div>
+              <div class="swiper-slide px-5"><img src="{{ asset('img/brands/c9.png') }}" alt="" /></div>
+              <div class="swiper-slide px-5"><img src="{{ asset('img/brands/c10.png') }}" alt="" /></div>
+              <div class="swiper-slide px-5"><img src="{{ asset('img/brands/c11.png') }}" alt="" /></div>
+            </div>
+            <!--/.swiper-wrapper -->
+          </div>
+          <!-- /.swiper -->
+        </div>
+        <!-- /.swiper-container -->
+      </div>
+      <!-- /.container -->
+    </section>
+    <!-- /section -->
+    <!-- /section -->
+
     <!-- /section -->
     <section class="wrapper bg-light">
       <div class="container pt-19 pb-14 pt-md-20 pb-md-16">
@@ -519,12 +541,12 @@
           <div class="col-md-8 col-lg-6 offset-lg-0 col-xl-5 offset-xl-1 position-relative">
             <div class="shape bg-dot primary rellax w-17 h-21" data-rellax-speed="1" style="top: -2rem; left: -1.4rem;">
             </div>
-            <figure class="rounded"><img src="{{ asset('img/photos/about4.jpg') }}"
-                 alt=""></figure>
+            <figure class="rounded"><img src="{{ asset('img/photos/about4.jpg') }}" alt=""></figure>
           </div>
           <!--/column -->
           <div class="col-lg-6">
-            <img src="{{ asset('img/icons/lineal/telemarketer.svg') }}" class="svg-inject icon-svg icon-svg-md mb-4" alt="" />
+            <img src="{{ asset('img/icons/lineal/telemarketer.svg') }}" class="svg-inject icon-svg icon-svg-md mb-4"
+              alt="" />
             <h2 class="display-4 mb-8">Tertarik? Hubungi Kami</h2>
             <div class="d-flex flex-row">
               <div>
@@ -562,7 +584,7 @@
       <!-- /.container -->
     </section>
     <!-- /section -->
-    
+
     <!-- /section -->
   </div>
   <!-- /.content-wrapper -->
@@ -571,8 +593,7 @@
       <div class="row gy-6 gy-lg-0">
         <div class="col-lg-4">
           <div class="widget">
-            <img class="mb-4" src="{{ asset('img/favicon-lugas.png') }}"
-              alt="" />
+            <img class="mb-4" src="{{ asset('img/favicon-lugas.png') }}" alt="" />
             <p class="mb-4">© <script>
                 document.write(new Date().getUTCFullYear());
               </script> Lugas. All rights reserved.</p>
@@ -601,9 +622,9 @@
           <!-- /.widget -->
         </div>
         <!-- /column -->
-        
+
         <!-- /column -->
-        
+
         <!-- /column -->
       </div>
       <!--/.row -->
