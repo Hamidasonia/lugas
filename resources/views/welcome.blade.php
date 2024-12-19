b93e04df6f1770ff33e98f483fccc6d8ee757635
Hamidasonia <hamidasonia11@gmail.com>
2024-12-19 22:21:28 +0700

    update new ui version

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
   </section>
 
   <!-- Section 1 -->
-  <section class="wrapper bg-light angled lower-end">
+  <section class="wrapper bg-light angled lower-end" id="home">
     <div class="container py-14 py-md-16">
       <div class="row gx-lg-8 gx-xl-12 gy-10 align-items-center mb-14 mb-md-18">
         <div class="col-lg-6 position-relative">
@@ -47,10 +47,10 @@
         </div>
         <!--/column -->
         <div class="col-lg-6">
-          <img src="{{ asset('img/icons/lineal/megaphone.svg') }}" class="svg-inject icon-svg icon-svg-md mb-4"
+          <img src="{{ asset('img/icons/lineal/megaphone.svg') }}" class="svg-inject icon-svg icon-svg-md text-primary mb-4" 
             alt="" />
-          <h2 class="display-4 mb-3">Solusi Pembangunan Kandang, Manajemen dan Konsultasi Ayam Petelur</h2>
-          <p class="lead fs-lg">Kami berfokus pada bidang ayam petelur dan menjadi ahli sejak tahun 1997</p>
+          <h2 class="display-4 mb-3" data-cue="slideInDown" style="color: #DC5800">Solusi Pembangunan Kandang, Manajemen dan Konsultasi Ayam Petelur</h2>
+          <p class="lead fs-lg" data-cue="slideInDown" style="color: #0D1E41">Kami berfokus pada bidang ayam petelur dan menjadi ahli sejak tahun 1997</p>
           <a href="#" class="btn btn-primary rounded-pill mb-0">Selanjutnya</a>
           <!--/.row -->
         </div>
@@ -60,7 +60,7 @@
   </section>
 
   <!-- Section 2 -->
-  <section class="wrapper bg-light">
+  {{-- <section class="wrapper bg-light">
     <div class="container pt-7 pt-md-10 pb-14 pb-md-17">
       <div class="row mb-5">
         <div class="col-md-10 col-xl-8 col-xxl-7 mx-auto text-center">
@@ -178,13 +178,105 @@
         </div>
       </div>
     </div>
+  </section> --}}
+
+  <section id="about" class="wrapper position-relative min-vh-60 d-lg-flex align-items-center" style="background-color: #0D1E41">
+    <div class="col-lg-6 position-lg-absolute top-0 start-0 image-wrapper bg-image bg-cover h-100" data-cue="slideInLeft" data-image-src="{{ asset('img/photos/farmer.jpg') }}">
+      <div class="divider divider-v-end d-none d-lg-block" style="color:#0D1E41">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 54 1200">
+          <g />
+          <g>
+            <g>
+              <polygon fill="currentColor" points="48 0 0 0 48 1200 54 1200 54 0 48 0" />
+            </g>
+          </g>
+        </svg>
+      </div>
+    </div>
+    <!--/column -->
+    <div class="container">
+      <div class="row gx-0">
+        <div class="col-lg-6 ms-auto">
+          <div class="pt-13 pb-15 pb-md-17 py-lg-16 ps-lg-15 pe-xxl-16">
+            <h2 class="fs-15 text-uppercase text-muted mb-3" data-cue="slideInLeft">Apa yang Kami Kerjakan?</h2>
+            <h3 class="display-3 ls-sm mb-7" style="color: #DC5800" data-cue="slideInLeft">Konsultasikan Kebutuhan Anda Seputar Bisnis Ayam Petelur dari Hulu Hingga Hilir Kepada Kami</h3>
+            <div class="d-flex flex-row mb-5">
+              <div>
+                <img src="{{ asset('img/icons/lineal/light-bulb.svg') }}" class="svg-inject icon-svg icon-svg-md text-orange me-5 mt-1" alt="" />
+              </div>
+              <div>
+                <h4 class="fs-20 ls-sm" style="color: #ffffff" data-cue="slideInLeft">Konsultasi Bisnis Ayam Petelur</h4>
+                <p class="mb-0" style="color: #ffffff" data-cue="slideInLeft">Nulla vitae elit libero pharetra augue dapibus. Praesent commodo cursus.</p>
+              </div>
+            </div>
+            <div class="d-flex flex-row mb-5">
+              <div>
+                <img src="{{ asset('img/icons/lineal/pie-chart-2.svg') }}" class="svg-inject icon-svg icon-svg-md text-orange me-5 mt-1" alt="" />
+              </div>
+              <div>
+                <h4 class="fs-20 ls-sm" style="color: #ffffff" data-cue="slideInLeft">Pembuatan Kandang dan Penyediaan Alat</h4>
+                <p class="mb-0" style="color: #ffffff" data-cue="slideInLeft">Vivamus sagittis lacus vel augue laoreet. Etiam porta sem malesuada magna.</p>
+              </div>
+            </div>
+            <div class="d-flex flex-row">
+              <div>
+                <img src="{{ asset('img/icons/lineal/design.svg') }}" class="svg-inject icon-svg icon-svg-md text-orange me-5 mt-1" alt="" />
+              </div>
+              <div>
+                <h4 class="fs-20 ls-sm" style="color: #ffffff" data-cue="slideInLeft">Manajemen Kandang Ayam Petelur</h4>
+                <p class="mb-0" style="color: #ffffff" data-cue="slideInLeft">Cras mattis consectetur purus sit amet. Aenean lacinia bibendum nulla sed.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--/column -->
+      </div>
+      <!--/.row -->
+    </div>
+    <!-- /.container -->
   </section>
 
-  <section id="snippet-1" class="wrapper bg-light wrapper-border">
+  <section id="process" class="wrapper bg-light">
+    <div class="container py-14 py-md-16">
+      <h2 class="display-4 mb-3">Bagaimana Mewujudkannya?</h2>
+      <p class="lead fs-lg mb-8">Berikut Tahapan Dalam <span class="underline">Mewujudkan Mimpi</span> Menjadi Peternak Ayam Petelur</p>
+      <div class="row gx-lg-8 gx-xl-12 gy-6 process-wrapper line">
+        <div class="col-md-6 col-lg-3"> <span class="icon btn btn-circle btn-lg btn-soft-primary pe-none mb-4"><span class="number">01</span></span>
+          <h4 class="mb-1" data-cue="bounceIn">Proyeksi peluang bisnis</h4>
+          <p class="mb-0" data-cue="bounceIn">Memetakan kebutuhan domestik, membuat perencanaan keuangan, ketersediaan pakan, dan memastikan distribution channel</p>
+        </div>
+        <!--/column -->
+        <div class="col-md-6 col-lg-3"> <span class="icon btn btn-circle btn-lg btn-primary pe-none mb-4"><span class="number">02</span></span>
+          <h4 class="mb-1" data-cue="bounceIn">Membuat kandang</h4>
+          <p class="mb-0" data-cue="bounceIn">Perencanaan kandang yang disesuaikan dengan anggaran, kebutuhan, geografis dan alat yang diperlukan</p>
+        </div>
+        <!--/column -->
+        <div class="col-md-6 col-lg-3"> <span class="icon btn btn-circle btn-lg btn-soft-primary pe-none mb-4"><span class="number">03</span></span>
+          <h4 class="mb-1" data-cue="bounceIn">Manajemen kandang yang baik</h4>
+          <p class="mb-0" data-cue="bounceIn">Memastikan operasional kandang dapat berjalan dengan baik, Anda dapat menerapkan sistem kerja kami yang telah terbukti efisien</p>
+        </div>
+        <!--/column -->
+        <div class="col-md-6 col-lg-3"> <span class="icon btn btn-circle btn-lg btn-soft-primary pe-none mb-4"><span class="number">04</span></span>
+          <h4 class="mb-1" data-cue="bounceIn">Menghasilkan produk</h4>
+          <p class="mb-0" data-cue="bounceIn">Dengan sistem manajemen yang baik, Anda dapat menghasilkan produk yang baik dan sesuai linimasa</p>
+        </div>
+        <!--/column -->
+      </div>
+      <!--/.row -->
+    </div>
+    <!-- /.container -->
+  </section>
+  <!-- /section -->
+
+  <section id="portfolio" class="wrapper bg-light wrapper-border">
     <div class="container pt-15 pt-md-17 pb-13 pb-md-15">
-      <h2 class="display-4 mb-3 text-center">Portfolio</h2>
-      <p class="lead fs-lg mb-10 text-center px-md-16 px-lg-21 px-xl-0">Project Sukses Kami Dalam Membangun Kandang
-      </p>
+      <div class="row text-center">
+        <div class="col-lg-10 col-xl-7 col-xxl-6 mx-auto">
+          <h2 class="fs-15 text-uppercase text-muted mb-3" data-cue="fadeIn">Portfolio</h2>
+          <h3 class="display-3 ls-sm mb-10" data-cue="fadeIn">Project Sukses Kami Dalam Membangun Kandang</h3>
+        </div>
+        <!-- /column -->
+      </div>
       <div class="swiper-container blog grid-view mb-6" data-margin="30" data-dots="true" data-items-xl="3"
         data-items-md="2" data-items-xs="1">
         <div class="swiper">
@@ -270,8 +362,9 @@
     </div>
     <!-- /.container -->
   </section>
+
   <!-- section -->
-  <section class="wrapper bg-gradient-primary">
+  <section id="proyeksi" class="wrapper bg-gradient-primary">
     <div class="container pt-10 pt-md-14 pb-8 text-center">
       <div class="row gx-lg-8 gx-xl-12 gy-10 align-items-center">
         <div class="col-lg-7">
@@ -292,8 +385,65 @@
     </div>
     <!-- /.container -->
   </section>
+
+  <section id="visi" class="wrapper bg-white">
+    <div class="container py-15 py-md-17">
+      <div class="text-center">
+        <h2 class="text-uppercase text-muted mb-3">Siapa kami?</h2>
+        <h1 class="ls-sm mb-5">Perusahaan yang Memiliki Visi dalam Memajukan Peternak Ayam Petelur</h1>
+        <p class="mb-6">Visi kami adalah memampukan para peternak ayam petelur untuk bersaing secara nasional, dengan memastikan efisiensi produksi dan keberlanjutan operasional</p>
+        
+        <!--/.row -->
+      </div>
+      <!--/.row -->
+      <div class="row gx-lg-8 gx-xl-12 gy-6 mb-15 mb-md-18">
+        <div class="col-lg-4">
+          <div class="d-flex flex-row">
+            <div>
+              <i class="uil uil-check"></i>
+            </div>
+            <div>
+              <h4 class="fs-20 ls-sm">01</h4>
+              <p class="mb-2">Mengembangkan prasarana peternakan untuk mendukung peningkatan produksi yang optimal.</p>
+            </div>
+          </div>
+        </div>
+        <!--/column -->
+        <div class="col-lg-4">
+          <div class="d-flex flex-row">
+            <div>
+              <i class="uil uil-check"></i>
+            </div>
+            <div>
+              <h4 class="fs-20 ls-sm">02</h4>
+              <p class="mb-2">Menyediakan layanan konsultasi dengan memberikan panduan dalam produksi yang efisien dan terstandar</p>
+            </div>
+          </div>
+        </div>
+        <!--/column -->
+        <div class="col-lg-4">
+          <div class="d-flex flex-row">
+            <div>
+              <i class="uil uil-check"></i>
+            </div>
+            <div>
+              <h4 class="fs-20 ls-sm">03</h4>
+              <p class="mb-2">Menyediakan layanan manajemen yang memastikan keberlanjutan operasional dengan efisiensi tinggi</p>
+            </div>
+          </div>
+        </div>
+        <!--/column -->
+      </div>
+      <div class="text-center">
+        <h3>Kami menerapkan standar tinggi dalam seluruh aspek pembangunan, mempekerjakan tenaga ahli yang profesional, dan menggunakan peralatan modern untuk memastikan hasil yang baik.</h3>
+      </div>
+      <!-- /.row -->
+    </div>
+    <!-- /.container -->
+  </section>
+
   <!-- /section -->
-  <section class="wrapper bg-light wrapper-border">
+  <section id="partner" class="wrapper bg-light wrapper-border">
     <div class="container py-14 py-md-16">
       <p class="text-center mb-8">Partner Kami</p>
       <div class="swiper-container clients mb-0" data-margin="30" data-dots="false" data-loop="true"
@@ -321,8 +471,9 @@
     </div>
     <!-- /.container -->
   </section>
+
   <!-- /section -->
-  <section class="wrapper bg-light">
+  <section id="faq" class="wrapper bg-gray">
     <div class="container py-14 py-md-16">
       <div class="row gx-lg-8 gx-xl-12 gy-10">
         <div class="col-lg-6 mb-0">
@@ -426,7 +577,7 @@
   </section>
   <!-- /section -->
 
-  <section class="wrapper bg-gray">
+  {{-- <section class="wrapper bg-gray">
     <div class="container pt-19 pb-14 pt-md-20 pb-md-16">
       <div class="row gx-md-8 gx-xl-12 gy-10 align-items-center">
         <div class="col-md-8 col-lg-6 offset-lg-0 col-xl-5 offset-xl-1 position-relative">
@@ -444,6 +595,21 @@
         <!--/column -->
       </div>
       <!--/.row -->
+    </div>
+    <!-- /.container -->
+  </section> --}}
+
+  <section class="wrapper image-wrapper bg-auto no-overlay bg-image text-center py-14 py-md-16 bg-map" data-image-src="{{ asset('img/map.png') }}">
+    <div class="container py-0 py-md-17">
+      <div class="row">
+        <div class="col-lg-6 col-xl-5 mx-auto">
+          <h2 class="display-4 mb-3 text-center">Tertarik?</h2>
+          <p class="lead mb-5 px-md-16 px-lg-3">Jangan ragu untuk menghubungi kami perihal kandang dan peternakan ayam petelur</p>
+          <a href="#" class="btn btn-primary rounded-pill">Konsultasi Sekarang</a>
+        </div>
+        <!-- /column -->
+      </div>
+      <!-- /.row -->
     </div>
     <!-- /.container -->
   </section>
